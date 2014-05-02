e12d84bc71f925219f7e45eaacd98c382435cace
Zack Sheppard <z@zack.io>
2014-05-02 08:58:06 -0400

    Program is close, still failing though

---
+++ leapyears.rb
@@ -0,0 +1,15 @@
+puts "Let's talk about leap years. I'll ask for a start and ending year, and I'll tell you all the leap years in between. First, choose a beginning year."
+year = gets.chomp.to_i
+
+puts "Great! Now how about that ending year?"
+ending_year = gets.chomp.to_i
+
+while year > ending_year
+	if 
+		year % 4 == 0 and 
+		year % 100 == 0 and 
+		year % 400 == 0 
+		puts year 
+	end
+	year = year - 1
+end
