a0eb28e1316cf218a9279447f2a9c41cfde58e49
Zack Sheppard <z@zack.io>
2014-05-02 16:57:41 -0400

    first commit for wordsort exercise

---
+++ wordsort.rb
@@ -0,0 +1,17 @@
+words = []
+puts "Type as many words as you want. I'll sort 'em for you."
+sleep(0.2)
+puts 
+puts "aaaaaaaand"
+sleep(0.5)
+puts "GO!"
+word = gets.chomp
+loop do
+	words << word
+	puts words.inspect
+	word = gets.chomp
+	break if word.nil? or word.empty?
+end
+puts "Nice, lets sort 'em"
+puts words.sort
+
